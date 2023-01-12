e7c72570d7b2018ae557f819cababd16e7f91bef
Callum Boddy <callum.boddy@collectingcars.com>
2023-01-12 10:57:52 +0000

    removed SwiftLint dependency

--- Package.swift
+++ Package.swift
@@ -14,9 +14,6 @@ let package = Package(name: "PushNotifications",
                                  .upToNextMajor(from: "9.2.0")),
                         .package(url: "https://github.com/AliSoftware/OHHTTPStubs",
                                  .upToNextMajor(from: "9.1.0")),
-                        // Source code linting
-                        .package(url: "https://github.com/realm/SwiftLint",
-                                 .upToNextMajor(from: "0.43.1"))
                       ],
                       targets: [
                         .target(name: "PushNotifications",
